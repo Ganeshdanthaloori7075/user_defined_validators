38304d4d59b156cefbf16341b2ae29e766bb7412
Ganeshdanthaloori <danthalooriganesh@gmail.com>
2023-02-06 15:58:01 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from django.core import validators
+
 def validate_for_a(value):
     if value[0]=='a':
         raise forms.ValidationError('name is started with a')
@@ -10,10 +12,11 @@ def check_for_len(value):
     if len(value)<5:
         raise forms.ValidationError('len is too low')
 class NameForm(forms.Form):
-    name=forms.CharField(max_length=100,validators=[validate_for_a,check_for_len])
+    name=forms.CharField(max_length=100,validators=[validators.MaxLengthValidator(5)])
     age=forms.IntegerField()
     email=forms.EmailField(max_length=100)
     reemail=forms.EmailField(max_length=100)
+    mobile=forms.CharField(max_length=10,min_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
     botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
 
 
